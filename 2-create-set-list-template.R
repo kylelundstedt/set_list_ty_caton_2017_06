03610916f4a9f1f1d11a20f231190917e5e15ff3
Kyle Lundstedt <klundstedt@industryvault.com>
2017-06-02 21:12:41 -0700

    add tweaks

--- 2-create-set-list-template.R
+++ 2-create-set-list-template.R
@@ -13,40 +13,28 @@ set_lists <- gs_title("LC Gigs Set Lists")
 gig_name <- "2017-06 Ty Caton"
 set_list <- set_lists %>% gs_read(ws = gig_name) %>%
   mutate( Artist = str_replace_all(Artist, "/", "-") ) %>%
-  mutate( Artist = str_replace_all(Artist, "&", "") ) %>%
+  mutate( Artist = str_replace_all(Artist, "’" , "") ) %>%
+  mutate( Artist = str_replace_all(Artist, "'", "") ) %>%
+  mutate( Artist = str_replace_all(Artist, "&", "n") ) %>%
+  mutate( Artist = str_replace_all(Artist, "\\.", "") ) %>%
+  mutate( Title = str_replace_all(Title, "/", "-") ) %>%
+  mutate( Title = str_replace_all(Title, "’" , "") ) %>%
+  mutate( Title = str_replace_all(Title, "'", "") ) %>%
+  mutate( Title = str_replace_all(Title, "&", "n") ) %>%
+  mutate( Title = str_replace_all(Title, "\\.", "") ) %>%
   mutate( Artist_Title =
            str_c("https://www.songlyrics.com/", Artist, "/", Title, "-lyrics/") ) %>% 
   mutate( Artist_Title = str_to_lower(Artist_Title) ) %>% 
-  mutate( Artist_Title = str_replace_all(Artist_Title, "\\s+", "-") )
-# create RMarkdown file for gig-specific Lead Sheets
-???
+  mutate( Artist_Title = str_replace_all(Artist_Title, "\\s+", "-") ) %>% 
+  mutate( Title_dash = str_replace_all(Title, " ", "-"))
 
+# create RMarkdown file for gig-specific Lead Sheets
 bookfilename <- str_c('book_filename: "', gig_name, '"\n', 'rmd_files: [')
 write(bookfilename, file = "_bookdown.yml")
-songs <- paste0('\t"',
-                 set_list$Title,
-                 '",')
+songs <- paste0('\t"lead_sheets/',
+                 set_list$Title_dash,
+                 '.Rmd",')
 write(songs, file = "_bookdown.yml", append = TRUE)
 outputs <- paste0('\n]\n',
                  'output_dir: docs')
 write(outputs, file = "_bookdown.yml", append = TRUE)
-
-html <- read_html(song_url)
-lyrics <- html %>%
-  html_nodes("#songLyricsDiv") %>%
-  html_text() %>%
-  str_replace_all("\n", "  \n") %>% 
-  str_replace_all("[\r]", "") %>% 
-  str_replace_all(c("€" = "", "â" = "", "½" = "", "" = ""))
-section <- paste0("# ", song_title, "\n")
-write(section, file = paste0("./", song_title, ".Rmd"))
-write(lyrics, file = paste0("./", song_title, ".Rmd"),
-      append = TRUE)
-
-# create RMarkdown file for Master Lead Sheets
-rmd <- list.files(path = "./Songs", pattern = '\\.md')
-chunks <- paste0("# ",
-                 tools::file_path_sans_ext(rmd),
-                 "\n```{r child = \"./Songs/", rmd, "\"}\n```\n\\clearpage\n")
-write(chunks, "chunks.Rmd")
-system("cat header.txt chunks.Rmd > Lead_Sheets_2017-05_Master.Rmd")
